bbd5b5203d844071f08d089e58e46864821c6d72
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2022-09-15 15:07:29 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+        int a, b, c;
+        printf("a: ");
+       scanf("%d", &a);
+        printf("b: ");
+      scanf("%d", &b);
+     c = a + b;
+       printf("a + b = %d\n", c);
+     return 0;
+}
+
